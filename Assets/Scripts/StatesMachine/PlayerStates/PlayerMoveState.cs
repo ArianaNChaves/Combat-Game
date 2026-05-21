bb1db92b428792cf7fc71c7dde3a49b7727f9d2c
ArianaNChaves <navi.ariana99@gmail.com>
2026-05-21 13:52:06 -0300

    add: player rotation and mouse movement

--- Assets/Scripts/StatesMachine/PlayerStates/PlayerMoveState.cs
+++ Assets/Scripts/StatesMachine/PlayerStates/PlayerMoveState.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class PlayerMoveState : PlayerBaseState
 {
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int IdleTime = Animator.StringToHash("IdleTime");
+    private const float AnimatorBlendValueMin = 0f;
+    private const float AnimatorBlendValueMax = 1f;
+    private const float MovementMagnitudeMin = 0.01f;
     private float _idleTimer = 0;
     public PlayerMoveState(PlayerStateMachine stateMachine) : base(stateMachine)
     {
@@ -15,27 +20,24 @@ public class PlayerMoveState : PlayerBaseState
 
     public override void Tick(float deltaTime)
     {
-        Vector3 movement = new Vector3();
-        movement.x = stateMachine.InputReader.MovementValue.x;
-        movement.z = stateMachine.InputReader.MovementValue.y;
-        movement.y = 0;
-        movement.Normalize();
-        
-        stateMachine.Controller.Move(movement * (stateMachine.MovementSpeed * deltaTime));
+        Vector3 movement = CalculateMovement();
 
-        if (stateMachine.InputReader.MovementValue == Vector2.zero) //no se mueve no se gira y aca activo la animacion de idle
+        stateMachine.Controller.Move(movement * (stateMachine.MovementSpeed * deltaTime));
+        if (stateMachine.InputReader.MovementValue.sqrMagnitude < MovementMagnitudeMin)
         {
             
-            _idleTimer += Mathf.SmoothStep(0f, 1f, deltaTime);
-            stateMachine.Animator.SetFloat("IdleTime", _idleTimer); 
-            stateMachine.Animator.SetFloat("Speed", stateMachine.InputReader.MovementValue.magnitude);
+            _idleTimer += Mathf.SmoothStep(AnimatorBlendValueMin, AnimatorBlendValueMax, deltaTime);
+            stateMachine.Animator.SetFloat(IdleTime, _idleTimer); 
+            stateMachine.Animator.SetFloat(Speed, AnimatorBlendValueMin);
             
             return;
         }
+
         _idleTimer = 0;
-        stateMachine.Animator.SetFloat("IdleTime", _idleTimer);
-        stateMachine.Animator.SetFloat("Speed", movement.magnitude);
-        stateMachine.transform.rotation = Quaternion.LookRotation(movement);
+        stateMachine.Animator.SetFloat(IdleTime, _idleTimer);
+        stateMachine.Animator.SetFloat(Speed, stateMachine.InputReader.MovementValue.sqrMagnitude);
+        
+        FaceMovementDirection(movement, deltaTime);
 
     }
 
@@ -43,4 +45,26 @@ public class PlayerMoveState : PlayerBaseState
     {
         
     }
+
+    private void FaceMovementDirection(Vector3 movementDirection, float deltaTime)
+    {
+        stateMachine.transform.rotation = Quaternion.Lerp(stateMachine.transform.rotation,
+            Quaternion.LookRotation(movementDirection), deltaTime * stateMachine.RotationSpeed);
+
+    }
+
+    private Vector3 CalculateMovement()
+    {
+        Vector3 forward = stateMachine.MainCameraTransform.forward;
+        Vector3 right = stateMachine.MainCameraTransform.right;
+        
+        forward.y = 0;
+        right.y = 0;
+        
+        forward.Normalize();
+        right.Normalize();
+        
+        return forward * stateMachine.InputReader.MovementValue.y + right * stateMachine.InputReader.MovementValue.x;
+    }
+    
 }
--- Assets/Scripts/StatesMachine/PlayerStates/PlayerStateMachine.cs
+++ Assets/Scripts/StatesMachine/PlayerStates/PlayerStateMachine.cs
@@ -5,11 +5,15 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public InputReader InputReader { get; private   set; }
     [field: SerializeField] public CharacterController Controller { get; private   set; }
     [field: SerializeField] public Animator Animator { get; private   set; }
+    public Transform MainCameraTransform { get; private   set; }
     
     [Header("Data")]
     [field: SerializeField] public float MovementSpeed { get; private   set; }
+    [field: SerializeField] public float RotationSpeed { get; private   set; } 
+
     private void Start()
     {
+        if (Camera.main != null) MainCameraTransform = Camera.main.transform;
         SwitchState(new PlayerMoveState(this));
     }
 
